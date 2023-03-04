78bb731fb7365789ab79391dd7581f690a3f78a3
FeliksHelenius <FeliksHelenius@gmail.com>
2023-03-05 00:53:24 +0100

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import axios from 'axios';
-import { Landing } from './pages/landing';
+import { Landing } from './pages/landing.jsx';
 import { Fragment } from 'react';
 
 function App() {
-  return (
-    <Fragment>
-      <Landing />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Landing />
+		</Fragment>
+	);
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-  },
+	base: '/vocab/',
+	plugins: [react()],
+	server: {
+		host: true,
+	},
 });
